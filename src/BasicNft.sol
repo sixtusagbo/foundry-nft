78b75f213e160fddc681e368d5a49ad8970d2d3b
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-02-05 20:16:49 +0100

    Setup basic NFT

---
+++ src/BasicNft.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie ", "DOG") {
+        s_tokenCounter = 0;
+    }
+}
