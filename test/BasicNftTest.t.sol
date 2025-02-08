5a927051dc40c832e7311500e0603c2d18e5ca7f
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-02-08 07:07:10 +0100

    Add tests for BasicNft

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    BasicNft basicNft;
+    DeployBasicNft deployer;
+    address public USER = makeAddr("user");
+    string public constant PUG = "ipfs://QmRgekUSD5sVUmiVqCSR29QRoNRxseWrBQR6UYyX9X34Eh?filename=foo-pug.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+        // assert(expectedName == actualName);
+        assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(basicNft.tokenURI(0))) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
+    }
+}
